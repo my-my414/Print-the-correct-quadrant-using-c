b04cf2dd615d0ef29ab6824a8b0fe3f5abffea86
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-09-04 14:11:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+
+{
+    int x,y;
+    printf("Enter a input : ");
+    scanf("%d%d",&x,&y);
+    if(x>0 &&  y>0)
+    {
+        printf("First quadrant");
+    }
+    else if(x<0 && y>0)
+    {
+        printf("Second quadrant");
+    }
+    else if(x<0 && y<0)
+    {
+        printf("Third quadrant");
+    }
+    else 
+    {
+        printf("Fouth quadrant");
+    }
+}
